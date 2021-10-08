3de064a0b6929a2a770a4b388620b79c472c8b80
YoTheDev <titoupriet@gmail.com>
2021-10-08 16:55:25 +0200

    Version2

---
+++ Assets/scripts/Turret.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Turret : MonoBehaviour
+{
+    private bool Realoaded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Realoaded = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.tag == "enemy" && Realoaded == false)
+        {
+            Destroy(GameObject.FindWithTag("enemy"));
+            Realoaded = true;
+            Invoke("Reload",2);
+        }
+    }
+
+    void Reload()
+    {
+        Realoaded = false;
+    }
+}
